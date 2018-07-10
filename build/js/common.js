e51312c9b79c0375013610bd59f8b1ef400f35cb
Rita Akhmadulina <rita.akhmadulina@gmail.com>
2018-07-10 21:53:13 +0300

    finish

--- build/js/common.js
+++ build/js/common.js
@@ -10,7 +10,13 @@ $(document).ready(function() {
     responsiveClass: true,
     responsive: {
       0: {
+        items: 2
+      },
+      400: {
         items: 3
+      },
+      480: {
+        items: 4
       },
       768: {
         items: 6
--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,4 @@
 //=include lib/jquery.js
-//=include lib/datatables.min.js
 //=include lib/rateit.js
 //=include lib/owl.carousel.min.js
 //=include common.js
